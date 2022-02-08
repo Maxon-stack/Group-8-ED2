d3ff6647998fc4d64bbb5c348f2d34c5c571afdd
Marlon Orellana <marlonorellana010@gmail.com>
2022-02-07 20:36:54 -0500

    added account creation logic with alert to inform user of success or error

--- Login/js/create_account.js
+++ Login/js/create_account.js
@@ -3,19 +3,38 @@
  * 
  */
 
-// Inputs
-const name = document.getElementById('userName');
-const username = document.getElementById('userEmail');
-const password = document.getElementById('userPassword');
-
 // Buttons
 const signupBtn = document.getElementById('signupBtn');
 const backToLoginBtn = document.getElementById('backToLoginBtn');
+const returnToDashboardBtn = document.getElementById('returnToDashboardBtn');
+
+// Form
+const signupForm = document.getElementById('signup-form');
 
 signupBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    console.log('Nothing yet!');
+    const adminName = signupForm['userName'].value;
+    const username = signupForm['userEmail'].value;
+    const password = signupForm['userPassword'].value;
+
+    const signupURL = `http://localhost:3000/signup?name=${adminName}&username=${username}&password=${password}`;
+
+    fetch(signupURL)
+        .then(response => {
+            response.json();
+            if (response.status === 201) {
+                alert('Account created successfully.');
+            } else if (response.status == 403) {
+                alert('An account with this email already exists.');
+            } else if (response.status === 500) {
+                alert('The server responded with an error.')
+            } else {
+                alert('Failed to create account.');
+            }
+        });
+
+    signupForm.reset();
 })
 
 // Navigate back to login page
@@ -23,4 +42,11 @@ backToLoginBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
     window.location.href = "index.html";
+});
+
+// Navigate back to home page
+returnToDashboardBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    window.location.href = "../../index.html";
 });
